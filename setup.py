b517aa88dc935aacf4019c513466884e40e9488b
toofikz2 <toofikz+2@wearehackerone.com>
2026-07-27 10:25:55 +0200

    clean setup for pipcompile

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 setup(name="depbot-impact-test", version="1.0.0",
-      install_requires=["requests==2.26.0", "urllib3==1.26.5"])
+      install_requires=["requests==2.25.0"])
