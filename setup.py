8911e71792acf36d20b63b8e798d3db8ac63907a
toofikz2 <toofikz+2@wearehackerone.com>
2026-07-27 11:00:32 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
-setup(name="depbot-impact-test", version="1.0.2",
-      install_requires=["requests==2.25.0"])
+setup(name="depbot-impact-test", version="1.0.0",
+      install_requires=["requests==2.31.0"])
